610f7b6ca415cfa9ef1380b1cc684b6e6eef9410
PrasetyoDwiki <prasetyodwiki48@gmail.com>
2021-03-16 10:26:44 +0700

    Create TugasMingguKe2_C.java

---
+++ Minggu 2/TugasMingguKe2_C.java
@@ -0,0 +1,16 @@
+
+package Tugas;
+
+public class TugasMingguKe2_C {
+   public static void main(String[] args) {
+       
+     // deklarasi variabel yang benar
+     
+     boolean gameOver = false;
+     int student = 50, classes = 3;
+     double sales_tax;
+     short number1;  
+     
+   }
+    
+}
