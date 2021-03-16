3d438c054d3d56767c7a570db9d3de0bc9628883
PrasetyoDwiki <prasetyodwiki48@gmail.com>
2021-03-16 10:26:39 +0700

    Create person.java

---
+++ Minggu 2/person.java
@@ -0,0 +1,27 @@
+
+package Tugas;
+
+
+public class person {
+    
+    String name;
+    int age;
+    
+    public person(String name,int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}
