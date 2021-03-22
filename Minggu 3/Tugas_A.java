ea641c2fb14e72392bb7b06535f7959e6fe93333
PrasetyoDwiki <prasetyodwiki48@gmail.com>
2021-03-23 01:45:37 +0700

    Update Tugas_A.java

--- Minggu 3/Tugas_A.java
+++ Minggu 3/Tugas_A.java
@@ -5,36 +5,20 @@
  */
 package Tugas;
 
-import java.util.Scanner;
-
 /**
  *
  * @author dwiki
  */
 public class Tugas_A {
+
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
-
-        System.out.print("Masukkan NIM : ");
-        String NIM = s.nextLine();
-
-        if (null == NIM) {
-            System.out.println("NIM yang anda masukan tidak ada");
+        String[] siswa1 = {"PRASETYO", "DWIKI", "NUGROHO"};
+        System.out.print("NAMA : ");
+        for (String data : siswa1) {
+            System.out.print(data + " ");
         }
-
-        switch (NIM) {
-            case "E41200694":
-                System.out.println("NIM\t: E41200694");
-                System.out.println("NAMA\t: PRASETYO DWIKI NUGROHO");
-                break;
-            case "E41200695":
-                System.out.println("NIM\t: E41200695");
-                System.out.println("NAMA\t: MUHAMMAD SHOLEH");
-                break;
-            default:
-                System.out.println("NIM yang anda masukan tidak ada");
-                break;
-        }
+        System.out.println("");
     }
-}
+  }
+
