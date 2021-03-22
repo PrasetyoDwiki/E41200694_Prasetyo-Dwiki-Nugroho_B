2a224dc3c1bd5515c3da6736798c83b82ec1405a
PrasetyoDwiki <prasetyodwiki48@gmail.com>
2021-03-23 00:49:58 +0700

    Create LatihanConditionalSentence.java

---
+++ Minggu 3/LatihanConditionalSentence.java
@@ -0,0 +1,53 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dwiki
+ */
+public class LatihanConditionalSentence {
+  public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("program kalkulator \n --------------------------");
+        System.out.print("Masukan nilai 1 : ");
+        int n1 = s.nextInt();
+        System.out.print("Masukan nilai 2 : ");
+        int n2 = s.nextInt();
+        System.out.println("1. +");
+        System.out.println("2. -");
+        System.out.println("3. *");
+        System.out.println("4. /");
+        System.out.println("5. %");
+        System.out.print("Masukan operator[1-5] : ");
+        s.nextLine();
+        String op = s.nextLine();
+        double hasil;
+        if ("+".equals(op)) {
+            hasil = n1 + n2;
+            System.out.println("Hasil : " + hasil);
+        } else if ("-".equals(op)) {
+            hasil = n1 - n2;
+            System.out.println("Hasil : " + hasil);
+        } else if ("*".equals(op)) {
+            hasil = n1 * n2;
+            System.out.println("Hasil : " + hasil);
+        } else if ("/".equals(op)) {
+            hasil = n1 / n2;
+            System.out.println("Hasil : " + hasil);
+        } else if ("%".equals(op)) {
+            hasil = n1 % n2;
+            System.out.println("Hasil : " + hasil);
+        } else {
+            System.out.println("operator yang anda masukin tidak ada");
+        }
+
+    }  
+}
