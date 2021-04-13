a572fac0018eaa54fd9c7ca967480223804a6b74
PrasetyoDwiki <prasetyodwiki48@gmail.com>
2021-04-13 12:37:21 +0700

    Create GraphApp.java

---
+++ Minggu 5/GraphApp.java
@@ -0,0 +1,28 @@
+
+package Tugas;
+
+/**
+ *
+ * @author dwiki
+ */
+public class GraphApp {
+    public static void main(String[] args) {
+        Latihan.Graph theGraph = new Latihan.Graph();
+        
+        theGraph.addVertex('A'); //0
+        theGraph.addVertex('B'); //1
+        theGraph.addVertex('C'); //2
+        theGraph.addVertex('D'); //3
+        theGraph.addVertex('E'); //4
+        
+        
+        
+        theGraph.addEdge(0, 1); //AB
+        theGraph.addEdge(1, 2); //BC
+        theGraph.addEdge(2, 3); //AD
+        theGraph.addEdge(3, 4); //DE
+        
+        
+        theGraph.bfs();
+    }
+}
