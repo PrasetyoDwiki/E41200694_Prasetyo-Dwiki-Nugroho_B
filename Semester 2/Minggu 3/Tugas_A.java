05603063545410fd69caa466a2e8e62345cba55d
PrasetyoDwiki <prasetyodwiki48@gmail.com>
2021-10-07 01:26:25 +0700

    Create Tugas_A.java

---
+++ Semester 2/Minggu 3/Tugas_A.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+
+/**
+ *
+ * @author dwiki
+ */
+public class Tugas_A {
+
+    public static void main(String[] args) {
+
+        String[] siswa1 = {"PRASETYO", "DWIKI", "NUGROHO"};
+        System.out.print("NAMA : ");
+        for (String data : siswa1) {
+            System.out.print(data + " ");
+        }
+        System.out.println("");
+    }
+  }
+
